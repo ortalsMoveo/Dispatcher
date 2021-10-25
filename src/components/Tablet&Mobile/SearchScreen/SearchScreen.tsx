a0ee806663856689ee56f52430e9a8c90f219254
ortal <ortals@moveo.co.il>
2021-10-25 19:53:14 +0300

    Delete stories files.

--- src/components/Tablet&Mobile/SearchScreen/SearchScreen.tsx
+++ src/components/Tablet&Mobile/SearchScreen/SearchScreen.tsx
@@ -1,17 +1,21 @@
 import RecentSearches,{Searches} from '../../Search/RecentSearches/RecentSearches';
 import Search,{SearchProps} from '../Search/Search';
 import {Container} from './SearchScreenStyle';
+import { ClearProps } from '../../Clear/Clear';
 
 interface SearchScreenProps  {
     searchProps: SearchProps;
     recentSearches: string[];
 }
+const clearButton: ClearProps={
+    smallScreen: false,
+  }
 
 const SearchScreen = ({searchProps, recentSearches}: SearchScreenProps) => {
     return (
         <Container>
             <Search searchIcon={searchProps.searchIcon} icon={searchProps.icon}/>
-             {/* <RecentSearches recentSearches={recentSearches}/> */}
+             <RecentSearches recentSearches={recentSearches} clearButton={clearButton}/>
         </Container>
     );
 }
