f5ad03f0f4aad1887178bfa9c4951601e712f439
ortalsMoveo <92311536+ortalsMoveo@users.noreply.github.com>
2021-11-14 14:13:45 +0200

    add Authentication user to app (#12)
    * add Authentication user to app
    
    * fixed PR notes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
 import MainPage from './fetchers/MainPage/MainPage';
 import WelcomePage from './fetchers/WelcomePage/WelcomPage';
-import { BrowserRouter as Router, Route, Switch,} from 'react-router-dom';
+import { Route, Switch,} from 'react-router-dom';
 import React from 'react';
 
 function App() {
   return (
-    <Router>
+    <>
       <Switch>
         <Route path="/home" component={MainPage} />
         <Route path="/" component={WelcomePage}/>
       </Switch>
-    </Router>
+    </>
   );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { NavbarContainer, IconsContainer, Nav, Logo} from './NavbarStyle';
+import { NavbarContainer, IconsContainer, Nav, Logo, Avatar, Icons} from './NavbarStyle';
 import Search from '../Search/Search';
 import LogoIcon from '../../assets/Group 1086418.svg';
 import settings from '../../assets/settings.svg';
@@ -6,6 +6,8 @@ import notifications from '../../assets/notifications.svg';
 import avatar from '../../assets/User avatar.svg';
 import React,{Dispatch} from 'react';
 import { FILTER_OPTIONS } from '../../FiltersData';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 export interface NavbarProps {
     recentSearches: string[];
@@ -14,6 +16,9 @@ export interface NavbarProps {
 }
 
 const Navbar = ({recentSearches, filterType, setFilterState}: NavbarProps) => {
+    const { logout, user } = useAuth0();
+    const { isAuthenticated } = useAuth0();
+   
     return(
         <NavbarContainer>
             <Nav>
@@ -25,9 +30,14 @@ const Navbar = ({recentSearches, filterType, setFilterState}: NavbarProps) => {
                 />
             </Nav>
             <IconsContainer>
-                <img src={settings}/> 
-                <img src={notifications}/>
-                <img src={avatar}/> 
+                <Icons>
+                    <img src={settings}/> 
+                    <img src={notifications}/>
+                </Icons>
+                <Avatar onClick={() =>
+                    logout({returnTo: window.location.origin})}>
+                    {user?.name?.charAt(0)}{user?.family_name?.charAt(0)}
+                </Avatar>
             </IconsContainer>
         </NavbarContainer>
     );
